24f66245adb9d912cc6b983d967e21ce79e796b1
Christopher Ponzio <chrisponz11@yahoo.com>
2022-03-12 15:50:47 -0500

    reorganized routes

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 //Dependencies
 const express = require('express');
+const path = require('path');
 
 //Routes
-const apiRoutes = require('./routes/apiRoutes');
-const htmlRoutes = require('./routes/htmlRoutes');
+const api = require('./routes/index.js');
+
 
 // Ports
 const PORT = process.env.PORT || 3001;
@@ -14,9 +15,24 @@ const app = express();
 //Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use('/api', apiRoutes);
-app.use('/', htmlRoutes);
+app.use('/api', api);
 app.use(express.static('public'));
+
+
+// GET Route for homepage
+app.get('/', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/index.html'))
+);
+
+// GET Route for notes page
+app.get('/notes', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/notes.html'))
+);
+
+// GET 404 error page for all others.
+app.all('*', (req, res) => {
+  res.status(404).send('<h1>404! Page not found</h1>');
+});
 
 app.listen(PORT, () =>
   console.log(`App listening at http://localhost:${PORT} 🚀`)
