88f9054422e1420fb528d59f7ff9fe66c4de6928
Christopher Ponzio <chrisponz11@yahoo.com>
2022-03-18 17:41:02 -0400

    readme added

--- README.md
+++ README.md
@@ -1,1 +1,30 @@
 # noteTaker
+
+  ![Github license](https://img.shields.io/badge/license-MIT-blue.svg)
+
+   ## Description
+    This project was designed to make personal or public notes and an easy to read lay out. Once you are finished with a note it can be deleted to make room for new notes.
+
+   ## Installation
+   To run this source code
+   * Heroku: https://https://stark-dawn-28882.herokuapp.com
+   * GitHub: https://christopherponzio.github.io/noteTaker
+   * Repository: https://github.com/ChristopherPonzio/noteTaker
+
+    ## Usage
+    * To use the NoteTaker you need to have Node.js, NPM, and Express.js installed.
+    * Once you open the file location in your terminal you can use "npm start" to start up a mock server to save notes to. 
+    * With the application running you can simply type in a title and the note. Click the Save button and the note will be saved!
+
+    * ![Demo Screenshot](./public/assets/images/noteTaker.png)
+
+    ## Credits
+    * Michelle Blackwell github: https://github/Mblackwellgca 
+    * https://stackoverflow.com 
+    * https://w3schools.com 
+    * https://choosealicense.com 
+    * https://img.shields.io
+
+    ## License
+    Licensed under the MIT license.
+    https://choosealicense.com/licenses/mit
--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -4,7 +4,7 @@ let saveNoteBtn;
 let newNoteBtn;
 let noteList;
 
-if (window.location.pathname === '/notes.html') {
+if (window.location.pathname === '/notes') {
   noteTitle = document.querySelector('.note-title');
   noteText = document.querySelector('.note-textarea');
   saveNoteBtn = document.querySelector('.save-note');
@@ -119,7 +119,7 @@ const handleRenderSaveBtn = () => {
 // Render the list of note titles
 const renderNoteList = async (notes) => {
   let jsonNotes = await notes.json();
-  if (window.location.pathname === '/notes.html') {
+  if (window.location.pathname === '/notes') {
     noteList.forEach((el) => (el.innerHTML = ''));
   }
 
@@ -165,7 +165,7 @@ const renderNoteList = async (notes) => {
     noteListItems.push(li);
   });
 
-  if (window.location.pathname === '/notes.html') {
+  if (window.location.pathname === '/notes') {
     noteListItems.forEach((note) => noteList[0].append(note));
   }
 };
@@ -173,7 +173,7 @@ const renderNoteList = async (notes) => {
 // Gets notes from the db and renders them to the sidebar
 const getAndRenderNotes = () => getNotes().then(renderNoteList);
 
-if (window.location.pathname === '/notes.html') {
+if (window.location.pathname === '/notes') {
   saveNoteBtn.addEventListener('click', handleNoteSave);
   newNoteBtn.addEventListener('click', handleNewNoteView);
   noteTitle.addEventListener('keyup', handleRenderSaveBtn);
